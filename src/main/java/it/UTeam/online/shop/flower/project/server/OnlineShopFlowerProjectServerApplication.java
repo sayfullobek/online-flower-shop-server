6db74a5f15ecf85d0a39b8ce93437e8955943ac9
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2024-02-01 00:14:01 -0800

    v1

---
+++ src/main/java/it/UTeam/online/shop/flower/project/server/OnlineShopFlowerProjectServerApplication.java
@@ -0,0 +1,13 @@
+package it.UTeam.online.shop.flower.project.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopFlowerProjectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopFlowerProjectServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/UTeam/online/shop/flower/project/server/OnlineShopFlowerProjectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.UTeam.online.shop.flower.project.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopFlowerProjectServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
